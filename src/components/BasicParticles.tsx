02d27114d5da15a69685c421cceb0387b2259a90
Raab Leopold <leopold.raab@extern.a1.at>
2023-04-19 16:54:37 +0200

    added BasicParticles component

---
+++ src/components/BasicParticles.tsx
@@ -0,0 +1,18 @@
+import { useRef } from 'react';
+import { Points } from 'three';
+
+const BasicParticles = () => {
+  // This reference gives us direct access to our points
+  const points = useRef<Points | null>(null);
+
+  // You can see that, like our mesh, points also takes a geometry and a material,
+  // but a specific material => pointsMaterial
+  return (
+    <points ref={points}>
+      <sphereGeometry args={[1.5, 108, 108]} />
+      <pointsMaterial color="#FAFAFA" size={0.008} sizeAttenuation />
+    </points>
+  );
+};
+
+export default BasicParticles;
