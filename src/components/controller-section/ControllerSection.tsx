342dd241ba2adc01896ce24a21c0b1767130e28d
Raab Leopold <leopold.raab@extern.a1.at>
2023-04-19 16:53:10 +0200

    added ControllerSection

---
+++ src/components/controller-section/ControllerSection.tsx
@@ -0,0 +1,15 @@
+import './ControllerSection.css';
+
+const ControllerSection = () => {
+  return (
+    <div className="controller--container">
+      <div className="controller--list">
+        <div className="controller--item">
+          <button className="controller--button">play</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ControllerSection;
